de7a21c05e90cbbd2240959b2e8c4d9602cf1594
jaem1n207 <jaem1n207@naver.com>
2020-01-02 02:00:54 +0900

    ADD ClickEvent

---
+++ Piano.js
@@ -0,0 +1,17 @@
+const keys = document.querySelectorAll(".key");
+
+keys.forEach(key => {
+  key.addEventListener("click", () => playNote(key));
+});
+
+function playNote(key) {
+  const noteAudio = document.getElementById(key.dataset.note);
+  noteAudio.currentTime = 0;
+  noteAudio.play();
+  key.classList.add("active");
+  console.log("hello");
+
+  noteAudio.addEventListener("ended", () => {
+    key.classList.remove("active");
+  });
+}
